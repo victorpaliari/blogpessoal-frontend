63105ffaacc475ab8e9555175a881af2636a028b
Victor Paliari <victorrpaliari@gmail.com>
2023-10-25 13:34:33 -0300

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css'
-
-import Home from './pages/home/Home'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
 
 function App() {
-    return (
-        <Home />
-
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>  
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
